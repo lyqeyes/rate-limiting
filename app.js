0659b719e0ac1519b4c1971a13df341cc1f11b22
刘衍晴 <liu3610yan@hotmail.com>
2018-01-05 19:01:42 +0800

    添加对本地和在线资源的代理

--- app.js
+++ app.js
@@ -3,3 +3,24 @@
  * @Date: 1/5/18
  */
 'use strict';
+const path = require('path');
+const koa = require('koa');
+const compress = require('koa-compress');
+const logger = require('koa-logger');
+const serve = require('koa-static');
+const bodyParser = require('koa-bodyparser');
+
+const controller = require('./router');
+
+const app = koa();
+
+app.use(bodyParser());
+app.use(serve(path.join(__dirname, 'public')));
+app.use(compress());
+app.use(logger());
+
+controller.register(app);
+
+app.listen(8080,()=>{
+    console.log('listening on port 8080');
+});
---
+++ router/home.js
@@ -0,0 +1,42 @@
+'use strict';
+var http = require('http');
+var https = require('https');
+
+let loadSource = (url)=>{
+    return new Promise(resolve => {
+        //http跟https要分开处理
+        (url.startsWith('https') ? https : http).get(url,res=>{
+            resolve(res);
+        })
+    });
+};
+
+var sleep = (time) => {
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve('9527');
+        },time);
+    });
+};
+
+//代理本地资源, 将资源置于/public目录下即可.
+var localResource = function* () {
+    this.params.url = `http://${this.get('host')}/${this.params.file}`;
+    yield onlineResource.apply(this);
+};
+
+//代理在线资源, 比如cdn上的脚本.
+var onlineResource = function* () {
+    let {url, time} = this.params;  //url会自动decode
+
+    let source = (yield [loadSource(url), sleep(time || 1000)])[0];
+
+    this.set(source.headers);
+
+    return this.body = source;
+};
+
+module.exports.register = function (router) {
+    router.get('/local/:file/:time', localResource);
+    router.get('/online/:url/:time', onlineResource);
+};
